840f06e28e147be9bbd47d19ce9a20453ad0e1f0
Sajal Agrawal <sajal.agrawal1997@gmail.com>
2017-06-21 19:11:16 +0530

    Create Sum of nodes at particular level.cpp

---
+++ Cpp/Sum of nodes at particular level.cpp
@@ -0,0 +1,7 @@
+//https://stackoverflow.com/questions/3020070/how-to-find-sum-of-nodes-value-for-given-depth-in-binary-tree
+
+
+sum(Node, Level) = 
+  if (Level == 0) return Node.value;
+  else return sum(Node.left, Level-1) + 
+              sum(Node.right, Level-1).
